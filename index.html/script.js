d4d8abf643bc006d6f062bb78b5124f1761b1e46
sangiakadam <sangitakadam118@proton.me>
2024-08-25 08:58:56 +0000

    functions

--- index.html/script.js
+++ index.html/script.js
@@ -5,3 +5,83 @@
 // console.log("12"+10)
 // con-cate-nation
 // console.log("hello"+ " "+ "sangita")
+// console.log("hello", prompt("enter your name"))
+
+// con-cate-nation
+
+// // console.log(prompt("enter any number ")+200)
+// let name=(prompt("enter any fruits"))
+// console.log((name),"juice")
+
+// let sub="Math"
+// sub="science"
+// console.log(sub)
+
+
+// let fruits
+// console.log(fruits+fruits) nan == not a number
+
+//  outout will be show undefined == data has blank
+
+// let userInput=Number(prompt("enter any number"))
+// console.log(userInput+12) 
+
+// variable -- storing data(that is not a fixed / changble)
+
+// 2) const= constant
+
+// const sub="Math"
+// sub="science"
+// console.log(sub)
+// // TypeError: Assignment to constant variable.(we can't change value /data)
+// both show nothing ness void
+// undefined == automaticall set (implicit)
+// null = manually set(explicit)
+
+// const a=null
+// console.log(10*a)  (implicit type conversion)
+// // type coercion
+// function sun(){
+//     console.log("hello");
+// }
+
+// sun()
+// sun()
+// sun()
+
+
+
+// scope - visibility in only function
+
+// global scope - visisbility in other than function
+
+
+// function sum(){
+// let a=2;
+// let b=3;
+// console.log(a+b)
+// }
+
+// sum()
+
+function sum1(num,num1)
+{
+// return num + num1;  return menace use to 
+}
+let a=sum1(1,2)
+console.log(a)
+
+function double(){
+    let a=3
+    console.log(a*2)
+}
+
+double()
+
+
+function double(){
+    let a=3
+return 
+}
+
+double()
