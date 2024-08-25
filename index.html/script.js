60d5ed525d825b17167c35fe168bf15058cab885
sangiakadam <sangitakadam118@proton.me>
2024-08-25 06:51:50 +0000

    javascript

---
+++ index.html/script.js
@@ -0,0 +1,7 @@
+// alert("hello")
+// console.log(prompt("enter your age"))
+// javascipt run js engin
+// console.log("wlecome","everyone")
+// console.log("12"+10)
+// con-cate-nation
+// console.log("hello"+ " "+ "sangita")
